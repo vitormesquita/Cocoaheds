6d98a34aba806a9c3714a287394e08882388cf88
Vitor Mesquita <vitor.mesquita09@gmail.com>
2017-02-13 19:06:52 -0300

    start impl

---
+++ Cocoaheads/Api/ApiClient.swift
@@ -0,0 +1,13 @@
+//
+//  ApiClient.swift
+//  Cocoaheads
+//
+//  Created by Vitor Mesquita on 13/02/17.
+//  Copyright © 2017 Vitor Mesquita. All rights reserved.
+//
+
+import UIKit
+
+class ApiClient: NSObject {
+
+}
